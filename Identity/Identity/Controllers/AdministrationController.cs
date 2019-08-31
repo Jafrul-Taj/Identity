9853183c32305cf5c5f240568ff9e25b1f85dd3f
Jafrul Hasan Taj <jfhasan210@gmail.com>
2019-09-01 04:38:28 +0600

    edit role

---
+++ Identity/Identity/Controllers/AdministrationController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Identity.ViewModels;
+using Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly UserManager<AplicationUser> userManager;
+
+        public AdministrationController(RoleManager<IdentityRole> roleManager,
+                                        UserManager<AplicationUser> userManager)
+        {
+            this.roleManager = roleManager;
+            this.userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                IdentityRole identityRole = new IdentityRole()
+                {
+                    Name = model.RoleName
+                };
+
+                IdentityResult result = await roleManager.CreateAsync(identityRole);
+
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("ListRole","Administration");
+                }
+                
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("",error.Description);
+                }
+            }
+            return View(model);
+        }
+
+        public IActionResult ListRole()
+        {
+            var roles = roleManager.Roles;
+            return View(roles);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditRole(EditRoleViewModel model)
+        {
+
+            IdentityRole role = await roleManager.FindByIdAsync(model.Id);
+
+            if(role==null)
+            {
+                ViewBag.ErrorMessage($"Role with id {model.Id} cannot be found");
+                return View("Not found");
+            }
+            else
+            {
+                role.Name = model.RoleName;
+
+                var result = await roleManager.UpdateAsync(role);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListRole", "Administration");
+                }
+
+                foreach(var errror in result.Errors)
+                {
+                    ModelState.AddModelError("", errror.Description);
+                }
+            }
+           
+            foreach(var user in userManager.Users)
+            {
+                if(await userManager.IsInRoleAsync(user, role.Name))
+                {
+                    model.Users.Add(user.UserName);
+                }
+            }
+            return View(model);
+        }
+        [HttpGet]
+        public async Task<IActionResult> EditRole(string id)
+        {
+
+            IdentityRole role = await roleManager.FindByIdAsync(id);
+
+            if(role==null)
+            {
+                ViewBag.ErrorMessage($"Role with id {id} cannot be found");
+                return View("Not found");
+            }
+            var model = new EditRoleViewModel()
+            {
+                Id = role.Id,
+                RoleName = role.Name
+
+            };
+
+            foreach(var user in userManager.Users)
+            {
+                if(await userManager.IsInRoleAsync(user, role.Name))
+                {
+                    model.Users.Add(user.UserName);
+                }
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Identity/Identity/Utilities/ValidEmailDomainAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity.Utilities
+{
+    public class ValidEmailDomainAttribute : ValidationAttribute
+    {
+        private readonly string alloweDomain;
+
+        public ValidEmailDomainAttribute(string alloweDomain)
+        {
+            this.alloweDomain = alloweDomain;
+        }
+
+        public override bool IsValid(object value)
+        {
+           string[] strings=  value.ToString().Split('@');
+           return  strings[1].ToUpper() == alloweDomain.ToUpper();
+        }
+    }
+}
