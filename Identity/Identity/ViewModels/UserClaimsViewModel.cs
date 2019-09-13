3a84cbbf2aab5d5fe33e8d3e6d0e413979985cfd
Jafrul Hasan Taj <jfhasan210@gmail.com>
2019-09-14 04:13:51 +0600

    Adding many files using add operation

---
+++ Identity/Identity/ViewModels/UserClaimsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Identity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Identity.ViewModels
+{
+    public class UserClaimsViewModel
+    {
+        public UserClaimsViewModel()
+        {
+            claim = new List<UserClaim>();
+        }
+        public string UserId { get; set; }
+        public List<UserClaim> claim { get; set; }
+
+    }
+}
---
+++ Identity/Identity/ViewModels/UserRolesViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity.ViewModels
+{
+    public class UserRolesViewModel
+    {
+        public string RoleId { get; set; }
+
+        public string RoleName { get; set; }
+
+        public bool isSeleted { get; set; }
+    }
+}
